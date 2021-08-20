e68fa5b65419850b37a4c68dd4fd08f16abc45f7
soparmar00 <88507243+soparmar00@users.noreply.github.com>
2021-08-20 09:51:20 +0530

    Add files via upload

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     return (
         <div>
             <ul>
-                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/'>Home</Link></li>
                 <li><Link to='/login'>Login</Link></li>
                 <li><Link to='/come'>Come Here</Link></li>
             </ul>
